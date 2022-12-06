2798f339389261bf34dc9360c3d316591131ec59
xdpiqbx <xdpiqbx@gmail.com>
2022-12-06 21:52:16 +0200

    added aadWorker enum Level

--- app/src/main/java/com/xdpiqbx/App.java
+++ app/src/main/java/com/xdpiqbx/App.java
@@ -7,10 +7,18 @@ import com.xdpiqbx.db.services.DatabaseInitService;
 import com.xdpiqbx.db.services.DatabasePopulateService;
 import com.xdpiqbx.db.services.DatabaseQueryService;
 
+import java.time.LocalDate;
+
 public class App {
     public static void main(String[] args) {
-        DatabaseInitService.initDatabase();
-        DatabasePopulateService.populateDatabase();
-        DatabaseQueryService.printResult();
+//        DatabaseInitService.initDatabase();
+//        DatabasePopulateService.populateDatabase();
+//        DatabaseQueryService.printResult();
+
+        DatabasePopulateService.addWorker(
+                "Viking",
+                LocalDate.now().minusYears(45),
+                DatabasePopulateService.Level.JUNIOR,
+                900);
     }
 }
--- app/src/main/java/com/xdpiqbx/db/services/DatabasePopulateService.java
+++ app/src/main/java/com/xdpiqbx/db/services/DatabasePopulateService.java
@@ -7,15 +7,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class DatabasePopulateService {
+    private static final Database db = Database.getInstance();
+    private static final Connection connection = db.getConnection();
     public static void main(String[] args) {
         populateDatabase();
     }
-    private static void populate(Database db, String pathToFile){
+    private static void populate(String pathToFile){
         try {
-            Connection connection = db.getConnection();
             String sql = String.join("\n", Files.readAllLines(Paths.get(pathToFile)));
             connection.prepareStatement(sql).executeUpdate();
         } catch (IOException | SQLException e) {
@@ -23,8 +26,42 @@ public class DatabasePopulateService {
         }
     }
 
+    public static void addWorker(String name, LocalDate birthday, DatabasePopulateService.Level level, int salary){
+        String sql = "INSERT INTO worker (name, birthday, level, salary)\n"
+                   + "  VALUES (?, ?, ?, ?)";
+        PreparedStatement insert = null;
+        try {
+            insert = connection.prepareStatement(sql);
+            insert.setString(1, name);
+            insert.setString(2, birthday.toString());
+            insert.setString(3, String.valueOf(level));
+            insert.setInt(4, salary);
+            insert.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void populateDatabase(){
         String initDbFile = Helper.env("SQL_FILES_PATH") + "populate_db.sql";
-        populate(Database.getInstance(), initDbFile);
+        populate(initDbFile);
+    }
+
+    public enum Level{
+        TRAINEE, JUNIOR, MIDDLE, SENIOR;
+        @Override
+        public String toString() {
+            switch (this){
+                case TRAINEE:
+                    return "Trainee";
+                case JUNIOR:
+                    return "Junior";
+                case MIDDLE:
+                    return "Middle";
+                case SENIOR:
+                    return "Senior";
+            }
+            return "ERROR";
+        }
     }
 }
