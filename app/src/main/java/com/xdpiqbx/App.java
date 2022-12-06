23a434fa551e337741b4ee51cae2b0b39b0a3628
xdpiqbx <xdpiqbx@gmail.com>
2022-12-06 21:19:47 +0200

    fix App init->Populate->print_res

--- app/src/main/java/com/xdpiqbx/App.java
+++ app/src/main/java/com/xdpiqbx/App.java
@@ -3,7 +3,14 @@
  */
 package com.xdpiqbx;
 
+import com.xdpiqbx.db.services.DatabaseInitService;
+import com.xdpiqbx.db.services.DatabasePopulateService;
+import com.xdpiqbx.db.services.DatabaseQueryService;
+
 public class App {
     public static void main(String[] args) {
+        DatabaseInitService.initDatabase();
+        DatabasePopulateService.populateDatabase();
+        DatabaseQueryService.printResult();
     }
 }
